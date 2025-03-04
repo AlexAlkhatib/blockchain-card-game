0089bd4f9dfffcbfdc518d815ae4bab49ccfe734
Olivier Neve <olivier.ch.neve@gmail.com>
2025-03-04 09:20:36 +0100

    made a qucik fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
 
     try {
       const generator = new CardMetadataGenerator();
-      const metadata = await generator.createRandomCard();
+      const metadata = await generator.createTestCard();
 
       console.log("Random metadata is:", JSON.stringify(metadata));
 
